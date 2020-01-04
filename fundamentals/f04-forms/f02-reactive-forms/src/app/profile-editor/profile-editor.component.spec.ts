32b472f8fe14934b1a08d61ca9a502e94737f8ad
Girma Nigusse <xgirma@gmail.com>
2020-01-03 23:37:37 -0500

    reactive forms

--- fundamentals/f04-forms/f02-reactive-forms/src/app/profile-editor/profile-editor.component.spec.ts
+++ fundamentals/f04-forms/f02-reactive-forms/src/app/profile-editor/profile-editor.component.spec.ts
@@ -3,7 +3,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProfileEditorComponent } from './profile-editor.component';
 
-const formValueExpected = { firstName: 'Foo', lastName: 'Bar' };
+const formValueExpected = { firstName: 'Foo', lastName: 'Bar',
+  address: { street: '123 Durham Bulls', city: 'Durham', state: 'NC', zip: '27707' } };
+const patchValueExpected = { firstName: 'Nancy', lastName: '',
+  address: { street: '123 Drew Street', city: '', state: '', zip: '' } };
+const mixed = { firstName: 'Nancy', lastName: 'Bar',
+  address: { street: '123 Drew Street', city: 'Durham', state: 'NC', zip: '27707' } };
 
 describe('ProfileEditorComponent', () => {
   let component: ProfileEditorComponent;
@@ -27,21 +32,66 @@ describe('ProfileEditorComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should edit profile', async () => {
-    const firstName = fixture.nativeElement.querySelector('#firstName > input');
-    firstName.value = 'Foo';
-    firstName.dispatchEvent(new Event('input'));
+  it('should edit profile',  () => {
+    commonTest(fixture);
+  });
 
-    const lastName = fixture.nativeElement.querySelector('#lastName > input');
-    lastName.value = 'Bar';
-    lastName.dispatchEvent(new Event('input'));
+  it('should update partial form value', async () => {
+    const updateButton = fixture.nativeElement.querySelector('#updateProfile > button');
+    updateButton.dispatchEvent(new Event('click'));
 
     fixture.detectChanges();
     await fixture.whenStable();
 
     const value = fixture.nativeElement.querySelector('#formValue > span');
     const formValue = JSON.parse(value.textContent);
-    expect(formValue).toEqual(jasmine.objectContaining(formValueExpected));
+    expect(formValue).toEqual(jasmine.objectContaining(patchValueExpected));
+  });
 
+  it('should only update required fields', async () => {
+    await commonTest(fixture);
+
+    const updateButton = fixture.nativeElement.querySelector('#updateProfile > button');
+    updateButton.dispatchEvent(new Event('click'));
+
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const valueAfterUpdate = fixture.nativeElement.querySelector('#formValue > span');
+    const formValueAfterUpdate = JSON.parse(valueAfterUpdate.textContent);
+    expect(formValueAfterUpdate).toEqual(jasmine.objectContaining(mixed));
   });
 });
+
+async function commonTest(fixture) {
+  const firstName = fixture.nativeElement.querySelector('#firstName > input');
+  firstName.value = 'Foo';
+  firstName.dispatchEvent(new Event('input'));
+
+  const lastName = fixture.nativeElement.querySelector('#lastName > input');
+  lastName.value = 'Bar';
+  lastName.dispatchEvent(new Event('input'));
+
+  const street = fixture.nativeElement.querySelector('#street > input');
+  street.value = '123 Durham Bulls';
+  street.dispatchEvent(new Event('input'));
+
+  const city = fixture.nativeElement.querySelector('#city > input');
+  city.value = 'Durham';
+  city.dispatchEvent(new Event('input'));
+
+  const state = fixture.nativeElement.querySelector('#state > input');
+  state.value = 'NC';
+  state.dispatchEvent(new Event('input'));
+
+  const zip = fixture.nativeElement.querySelector('#zip > input');
+  zip.value = '27707';
+  zip.dispatchEvent(new Event('input'));
+
+  fixture.detectChanges();
+  await fixture.whenStable();
+
+  const value = fixture.nativeElement.querySelector('#formValue > span');
+  const formValue = JSON.parse(value.textContent);
+  expect(formValue).toEqual(jasmine.objectContaining(formValueExpected));
+}
--- fundamentals/f04-forms/f02-reactive-forms/src/app/profile-editor/profile-editor.component.ts
+++ fundamentals/f04-forms/f02-reactive-forms/src/app/profile-editor/profile-editor.component.ts
@@ -10,6 +10,12 @@ export class ProfileEditorComponent implements OnInit {
   profileForm = new FormGroup({
     firstName: new FormControl(''),
     lastName: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl(''),
+      city: new FormControl(''),
+      state: new FormControl(''),
+      zip: new FormControl('')
+    })
   });
 
   constructor() { }
@@ -20,6 +26,15 @@ export class ProfileEditorComponent implements OnInit {
   onSubmit() {
     // TODO: Use EventEmitter with form value
     console.warn(this.profileForm.value);
+  }
+
+  updateProfile() {
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
   }
 
 }
