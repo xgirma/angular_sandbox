4c657155eb815405ab1c68127e1fdf8af2dd09c9
Girma Nigusse <xgirma@gmail.com>
2020-01-05 03:51:17 -0500

    form validation

--- fundamentals/f04-forms/f04-form-validation/src/app/template-driven/template-driven.component.spec.ts
+++ fundamentals/f04-forms/f04-form-validation/src/app/template-driven/template-driven.component.spec.ts
@@ -1,14 +1,21 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import { NgForm, FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 import { TemplateDrivenComponent } from './template-driven.component';
+
 
 describe('TemplateDrivenComponent', () => {
   let component: TemplateDrivenComponent;
   let fixture: ComponentFixture<TemplateDrivenComponent>;
+  let form: NgForm;
+  let compiled: any;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ TemplateDrivenComponent ]
+      declarations: [ TemplateDrivenComponent ],
+      imports: [ FormsModule ],
+      providers: [NgForm]
     })
     .compileComponents();
   }));
@@ -16,10 +23,68 @@ describe('TemplateDrivenComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TemplateDrivenComponent);
     component = fixture.componentInstance;
+    const formElement = fixture.debugElement.query(By.css('form'));
+    form = formElement.injector.get(NgForm);
     fixture.detectChanges();
+    compiled = fixture.debugElement.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.hero).toBeTruthy();
+    expect(component.powers).toBeTruthy();
   });
+
+  it('should submit with valid name', async () => {
+    const name = 'Mr. Boos';
+    fixture.componentInstance.hero.name = name;
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(component.hero.name).toEqual(name);
+    expect(form.submitted).toBeFalsy();
+
+    const submit = compiled.querySelector('#submitBtn');
+    submit.click();
+    fixture.detectChanges();
+    await fixture.whenStable();
+    expect(form.submitted).toBeTruthy();
+
+    const message = compiled.querySelector('#newHero > p');
+    const addBtn = compiled.querySelector('#newHero > button');
+    expect(message.textContent).toEqual(`You've submitted your hero, ${name}!`);
+    expect(addBtn).toBeDefined();
+  });
+
+  it('should have name length > 4 character', async () => {
+    fixture.componentInstance.hero.name = 'Foo';
+    fixture.detectChanges();
+
+    expect(form.submitted).toBeFalsy();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      const submit = fixture.debugElement.query(By.css('#submitBtn')).nativeElement;
+      expect(submit.disabled).toBeTruthy();
+
+      const control = form.control.get('name');
+      expect(control.hasError('minlength')).toBe(true);
+    });
+  });
+
+  it('should have name', async () => {
+    fixture.componentInstance.hero.name = '';
+    fixture.detectChanges();
+
+    expect(form.submitted).toBeFalsy();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      const submit = fixture.debugElement.query(By.css('#submitBtn')).nativeElement;
+      expect(submit.disabled).toBeTruthy();
+
+      const control = form.control.get('name');
+      expect(control.hasError('required')).toBe(true);
+    });
+  });
+
+  // TODO
+  xit('should reset', () => {});
 });
