4f83e5d88b5ee2568314f59a93d443811c0e9f93
Girma Nigusse <xgirma@gmail.com>
2020-01-06 21:26:26 -0500

    dynamic form

--- fundamentals/f04-forms/f05-dynamic-form/src/app/app.component.ts
+++ fundamentals/f04-forms/f05-dynamic-form/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { QuestionService } from './question.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:  [QuestionService]
 })
 export class AppComponent {
-  title = 'f05-dynamic-form';
+  questions: any[];
+
+  constructor(service: QuestionService) {
+    this.questions = service.getQuestions();
+  }
 }
--- fundamentals/f04-forms/f05-dynamic-form/src/app/app.module.ts
+++ fundamentals/f04-forms/f05-dynamic-form/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicFormComponent,
+    DynamicFormQuestionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
