f5465d0bf0e0c3a5ad0e9fbad93251531af31f06
Girma Nigusse <xgirma@gmail.com>
2020-01-09 20:13:53 -0500

    dev workflow - testing

--- dev-workflow/d04-testing/src/app/demo/demo.spec.ts
+++ dev-workflow/d04-testing/src/app/demo/demo.spec.ts
@@ -74,3 +74,22 @@ describe('MasterService without Angular testing support', () => {
     expect(valueServiceSpy.getValue.calls.mostRecent().returnValue).toBe(stubValue);
   });
 });
+
+function setup() {
+  const valueServiceSpy = jasmine.createSpyObj('ValueService', ['getValue']);
+  const stubValue = 'stub value';
+  const masterService = new MasterService(valueServiceSpy);
+
+  valueServiceSpy.getValue.and.returnValue(stubValue);
+  return { masterService, stubValue, valueServiceSpy };
+}
+
+describe('MasterService without beforeEach', () => {
+  it(`#getValue should return stubbed value from a spy`, () => {
+    const { masterService, stubValue, valueServiceSpy } = setup();
+
+    expect(masterService.getValue()).toBe(stubValue, 'service returned stub value');
+    expect(valueServiceSpy.getValue.calls.count()).toBe(1, 'spy method was called once');
+    expect(valueServiceSpy.getValue.calls.mostRecent()).toBe(stubValue, '');
+  });
+});
